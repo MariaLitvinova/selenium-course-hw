3a74ecbb8b134b543528a8a5410fa3b7cc6469ba
MariaLitvinova <mariia.v.litvinova@gmail.com>
2019-08-07 14:00:53 +0300

    добавлен сценарий для логина в панель администрирования

---
+++ litecart/LitecartTesting/LitecartTesting/LoginTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace LitecartTesting
+{
+    [TestFixture]
+    public class LoginTest
+    {
+        private IWebDriver webDriver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void SetUp()
+        {
+            webDriver = new ChromeDriver();
+            wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(20));
+        }
+
+        [Test]
+        public void LoginWithCorrectCredentials()
+        {
+            webDriver.Url = "http://localhost/litecart/admin/";
+
+            webDriver.FindElement(By.Name("username")).SendKeys("admin");
+            webDriver.FindElement(By.Name("password")).SendKeys("admin");
+
+            webDriver.FindElement(By.Name("login")).Click();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            webDriver.Quit();
+            webDriver = null;
+        }
+    }
+}
---
+++ litecart/LitecartTesting/LitecartTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LitecartTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LitecartTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c5319f31-d135-4da5-b967-a9406e49a205")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
